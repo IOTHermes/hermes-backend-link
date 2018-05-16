aa6542e5978804a3b73f33118dbdd4b0514d21be
Pedro Lima <pedro.mlmonteiro@gmail.com>
2018-05-16 20:25:31 +0100

    Added first (dummy) test file.

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+const assert = chai.assert;
+
+describe('Hermes Backend Link Tests', () => {
+  it('First test', done => {
+    done();
+  });
+});
